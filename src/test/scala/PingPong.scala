05c0c1dc245c5f04ac6fcc0a464899dea29666ee
sebastian.harko <sebastian.harko@lightbend.com>
2017-10-28 17:48:00 +0200

    add transaction simulation

--- src/test/scala/PingPong.scala
+++ src/test/scala/PingPong.scala
@@ -22,3 +22,30 @@ class Ping extends Simulation {
 
 }
 
+
+class TransactionOnly extends Simulation {
+
+  val feeder: Iterator[Map[String, Any]] = Iterator.continually {
+    val transactionId = java.util.UUID.randomUUID().toString.replace("-", "").substring(0, 15)
+    val from = java.util.UUID.randomUUID().toString.replace("-", "").substring(0, 15)
+    val to = java.util.UUID.randomUUID().toString.replace("-", "").substring(0, 15)
+    Map("from" -> from, "to" -> to, "transactionId" -> transactionId)
+  }
+
+  val host = sys.env.getOrElse("APP_IP_PORT", "localhost:8080")
+  val users = sys.env.get("USERS").map(_.toInt).getOrElse(50)
+  val time = sys.env.get("PERIOD").map(_.toInt).getOrElse(10)
+
+
+  def action: String = "transaction"
+
+  val httpConf: HttpProtocolBuilder = http
+
+  val scn1: ScenarioBuilder = scenario("One transaction").feed(feeder)
+      .exec(
+        http("transaction").post(session => s"http://$host/$action/${session("transactionId").as[String]}/${session("from").as[String]}/${session("to").as[String]}/0")
+            .check(substring("true")))
+
+  setUp(scn1.inject(constantUsersPerSec(users) during (time minutes))).protocols(httpConf)
+
+}
